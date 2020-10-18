a3d8c0a1a5b0c13c1f548e0f6388ecc736b20afe
Luan Santos <luansantos@iMac-Pro-de-Luan.local>
2020-10-18 12:12:02 -0700

    tabbar for items products

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -9,6 +9,28 @@ class ProductsScreens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(snapshot.data['title']),
+            centerTitle: true,
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(icon: Icon(Icons.grid_on),),
+                Tab(icon: Icon(Icons.list),),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              Container(color: Colors.grey,),
+              Container(color: Colors.blue,),
+            ],
+          ),
+        )
+    );
   }
 }
