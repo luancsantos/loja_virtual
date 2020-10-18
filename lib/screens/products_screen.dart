9dffe0425da90cf8e95bd2372c7d1f1976c93c8f
Luan Santos <luansantos@iMac-Pro-de-Luan.local>
2020-10-18 12:06:02 -0700

    passed category document

---
+++ lib/screens/products_screen.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ProductsScreens extends StatelessWidget {
+
+  final DocumentSnapshot snapshot;
+
+  ProductsScreens(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
