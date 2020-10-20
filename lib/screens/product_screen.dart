e4aa11caa9ad3c03e94f8a6834b8a0e665e5f13c
Luan Santos <luansantos@iMac-Pro-de-Luan.local>
2020-10-20 00:21:32 -0700

    productscreen with images

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,10 +1,48 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virutal_app/datas/product_data.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
+  
+  final ProductData product;
+  ProductScreen(this.product);
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState(product);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+
+  final ProductData product;
+  _ProductScreenState(this.product);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('teste'),
+
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+       appBar: AppBar(
+         title: Text(product.title),
+         centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((e) {
+                return NetworkImage(e);
+              }).toList(),
+              dotSize: 4.0,
+              dotSpacing: 15.0,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              autoplay: false,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
