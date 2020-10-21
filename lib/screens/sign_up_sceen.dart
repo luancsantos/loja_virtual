d9f2acb1138a375648f5d89e21a71cbe4bcc758e
Luan Santos <luansantos@iMac-Pro-de-Luan.local>
2020-10-20 22:31:53 -0700

    create screen signUp

---
+++ lib/screens/sign_up_sceen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class SingUpScreen extends StatelessWidget {
+
+ final _formKey =  GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criar Conta'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Nome'
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text){
+                if (text.isEmpty) {
+                  return ' Informe o nome';
+                } 
+              },
+            ),
+            SizedBox(height: 16.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'E-mail'
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text){
+                if (text.isEmpty || !text.contains('@')) {
+                  return 'E-mail inválido';
+                } 
+              },
+            ),
+            SizedBox(height: 16.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Senha'
+              ),
+              obscureText: true,
+              validator: (text){
+                if (text.isEmpty || text.length < 6) {
+                  return 'Senha inválida';
+                } 
+              },
+            ),
+            SizedBox(height: 16.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Endereço'
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text){
+                if (text.isEmpty) {
+                  return ' Informe o endereço';
+                } 
+              },
+            ),
+            SizedBox(height: 16.0,),
+            SizedBox(
+              height: 40.0,
+              child: RaisedButton(
+                onPressed: (){
+                  if(_formKey.currentState.validate()){
+
+                  }
+                },
+                child: Text("Criar Conta",
+                style: TextStyle(fontSize: 18.0,)
+                ),
+                textColor: Colors.white,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
